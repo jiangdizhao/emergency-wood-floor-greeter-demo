c2044f1568856513215b05a26dc111e2674792f4
jiangdizhao <jiangdizhao@gmail.com>
2026-07-11 18:41:07 +1000

    Add face identity package

---
+++ backend/app/identity/__init__.py
@@ -0,0 +1,10 @@
+from .face_embedder import FaceEmbedder, FaceEmbeddingResult
+from .identity_service import IdentityService
+from .repository import IdentityRepository
+
+__all__ = [
+    "FaceEmbedder",
+    "FaceEmbeddingResult",
+    "IdentityRepository",
+    "IdentityService",
+]
