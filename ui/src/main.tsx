654b3cdd320e7f6eca7d5af2b9303513a691acc9
jiangdizhao <jiangdizhao@gmail.com>
2026-07-11 17:36:39 +1000

    Install domain-aware browser speech recognition patch

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './voice.css'
+import './speechRecognitionDomainPatch'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
